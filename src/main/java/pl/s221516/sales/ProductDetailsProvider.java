25c75be31bd25c470c2c48a7acbceca9a16b7c70
KrystianTaf <krystian.taf@gmail.com>
2023-05-15 15:42:00 +0200

    fix to sales module

--- src/main/java/pl/s221516/sales/ProductDetailsProvider.java
+++ src/main/java/pl/s221516/sales/ProductDetailsProvider.java
@@ -1,4 +1,9 @@
 package pl.s221516.sales;
 
+import java.util.Optional;
+
 public class ProductDetailsProvider {
+    public Optional<ProductDetails> load(String productId) {
+        return Optional.empty();
+    }
 }
